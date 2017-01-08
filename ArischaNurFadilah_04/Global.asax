6e5a1963b92b367768934bd2704dbc9b0c92b8a2
Faychan <nor.iffat32@gmail.com>
2017-01-08 11:17:21 +0700

    Add project files.

---
+++ ArischaNurFadilah_04/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArischaNurFadilah_04.MvcApplication" Language="C#" %>
---
+++ ArischaNurFadilah_04/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArischaNurFadilah_04.Startup))]
+namespace ArischaNurFadilah_04
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
